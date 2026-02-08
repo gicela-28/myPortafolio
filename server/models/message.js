49e5d20d7e7c516e85f3c7a37f5ef7e949cc9dad
unknown <gicy957@gmail.com>
2026-02-07 23:13:30 -0500

    corrigiendo configuracion

--- server/models/message.js
+++ server/models/message.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Definimos qué datos vamos a guardar de cada mensaje
 const MessageSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  message: { type: String, required: true },
-  date: { type: Date, default: Date.now } // Se guarda la fecha automáticamente
+	name: { type: String, required: true },
+	email: { type: String, required: true },
+	message: { type: String, required: true },
+	date: { type: Date, default: Date.now }, // Se guarda la fecha automáticamente
 });
 
-module.exports = mongoose.model('Message', MessageSchema);
+module.exports = mongoose.model("Message", MessageSchema);
